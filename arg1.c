43c7725702192a1a6c9b2411e37937454d13ab1e
username <dixonmichaelDX@gmail.com>
2021-04-27 14:15:22 +0530

    new changes

---
+++ arg1.c
@@ -0,0 +1,14 @@
+
+
+int main(int argc,char *argv[]){
+   int a=10;
+   int b=20;
+   if(a==b) {
+      printf("a is equal to b %d = %d\n",a,b);
+      else{
+         printf("a is not equal to b %d != %d\n",a,b);
+      }
+   }
+
+}
+   
